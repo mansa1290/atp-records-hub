6eae1ea7894ed41475eb9538867d5417044fb186
mansa1290 <goyalhimanshu1290@gmail.com>
2025-11-26 09:56:23 +0530

    Add next.config.js with basic configuration

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-UA-Compatible', value: 'IE=edge' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
